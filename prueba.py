1e5337b944834b1cfc53f379c7915942fb917c56
devops <devops@sct.gob.mx>
2020-02-11 16:15:54 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,2 +1,12 @@
-print("hola mundo")
+import os
+
+def fun():
+    num = int(os.environ["PY_NUM"])
+    mod = num%2
+    if mod==0;
+        print("Par")
+    else
+        print("Impar")
+if_name_=="_main_":
+fun()
 
