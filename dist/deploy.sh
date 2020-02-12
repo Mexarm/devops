c7100318df082c6acc22bf8aafe24c5cc8371019
Carlos Eduarco Simón Zepeda <casimon@redhat.com>
2020-02-12 12:39:01 -0600

    notas

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -1,3 +1,12 @@
+# template de construccion
 oc new-build --name java-binary --binary --strategy docker
+# compilacion (omitir)
+mvn clean install
+# mover compilacion hacia dir (omitir)
+cp target/test-wsdl2java-0.0.1.jar dist/test-wsdl2java-0.0.1.jar
+
+# comenzar la construccion de la imagen en ocp
+cd dist
 oc start-build java-binary --from-dir=.
-oc new-app --name java-test -i java-binary
+# crear un deployment de la imagen
+oc new-app --name java-test --image-stream="prueba-redhat/java-binary-1:latest"
