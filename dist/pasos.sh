aba57511967572b63e7cba58e48238ff889a9fe3
Carlos Eduarco Simón Zepeda <casimon@redhat.com>
2020-02-12 16:37:20 -0600

    dist docker openshift

---
+++ dist/pasos.sh
@@ -0,0 +1,21 @@
+# template de construccion
+oc new-build --name spring-binary --binary --strategy docker
+
+##################################################################
+# compilacion (omitir)
+mvn clean install
+# mover compilacion hacia dir (omitir)
+cp target/spring-api.jar dist/.
+##################################################################
+
+# comenzar la construccion de la imagen en ocp
+cd dist
+oc start-build spring-binary --from-dir=.
+# crear un deployment de la imagen
+oc new-app --name spring-test --image-stream="{project}/{is}:latest"
+
+
+# exponer la ruta
+oc expose svc/spring-test
+# obtener la url
+oc get routes
