a2809e64f95c573de03d853e09b3cd298cc0495a
Carlos Eduarco Simón Zepeda <casimon@redhat.com>
2020-02-13 12:09:33 -0600

    pasos

---
+++ pasos.sh
@@ -0,0 +1,8 @@
+# basado en python
+pip3 install django django_rest_framework
+# correr migraciones
+python manage.py migrate
+# crear super usuario de admin
+python manage.py createsuperuser
+# iniciar servidor
+python manage.py runserver
