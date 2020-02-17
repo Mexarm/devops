cc755fb277f8d8b0395c755951e4e6802a4b093e
Armando Hernandez Marin <armando.hernandez@infotec.mx>
2020-02-17 00:59:20 -0600

    settings.py

--- example/urls.py
+++ example/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from curso.views import AllCategory
+from curso.views import AllCategory, view2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
